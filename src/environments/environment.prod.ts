50cdaa2f8c5dd5de12f00a6331a9878662825b6c
paulphoku <paulphoku@gmail.com>
2020-08-01 16:40:59 +0200

    production ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyA33knB8mXn_B2t8wipGY5ht5hkC1YmRX0",
+    authDomain: "angufyrbse-app-4e675.firebaseapp.com",
+    databaseURL: "https://angufyrbse-app-4e675.firebaseio.com",
+    projectId: "angufyrbse-app-4e675",
+    storageBucket: "angufyrbse-app-4e675.appspot.com",
+    messagingSenderId: "889302759006",
+    appId: "1:889302759006:web:b39dac08c8c3219be966cf",
+    measurementId: "G-ERSS5X82EG"
+  }
 };
